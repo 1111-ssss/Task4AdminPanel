e2a6f1d73ea726de0ac4046977be74692a2b8292
1111-ssss <gurskievgeni@gmail.com>
2026-08-15 12:11:51 +0300

    feature(data): create user entity

---
+++ src/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+using Data.Interfaces;
+using Data.Enums;
+
+namespace Data.Entities;
+
+public class ApplicationUser : IEntity
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public DateTime RegistrationTime { get; set; }
+    public DateTime LastLoginTime { get; set; }
+    public UserStatus Status { get; set; } = UserStatus.Unverified;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string? EmailConfirmationToken { get; set; }
+    public DateTime? EmailConfirmationExpiration { get; set; }
+}
