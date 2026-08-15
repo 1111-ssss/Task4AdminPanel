289836899fa08ed638e2967be726e0939521c6e9
1111-ssss <gurskievgeni@gmail.com>
2026-08-15 12:22:56 +0300

    feature(business-services): add account register service

---
+++ src/Business/Interfaces/Account/IAccountRegisterService.cs
@@ -0,0 +1,9 @@
+using Ardalis.Result;
+using Business.Contracts.Account;
+
+namespace Business.Interfaces.Account;
+
+public interface IAccountRegisterService
+{
+    Task<Result<UserResponse>> Register(RegisterUserRequest request);
+}
