080806515d1268c3375b2764747a25e7c8a6daa0
1111-ssss <gurskievgeni@gmail.com>
2026-08-16 14:36:09 +0300

    fix(business-services): change minimal password length to 1

--- src/Business/Validators/Account/LoginUserValidator.cs
+++ src/Business/Validators/Account/LoginUserValidator.cs
@@ -13,7 +13,7 @@ public class LoginUserValidator : AbstractValidator<LoginUserRequest>
             .MaximumLength(256);
         RuleFor(x => x.Password)
             .NotEmpty()
-            .MinimumLength(8)
+            .MinimumLength(1)
             .MaximumLength(128);
     }
 }
